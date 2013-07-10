f2787cc668cf91b35ab25aa225fa8f4a31a80410
Audrey Roy <audreyr@gmail.com>
2013-07-10 09:30:00 +0200

    Refactor into functions.

--- serve.py
+++ serve.py
@@ -8,7 +8,6 @@ import socketserver
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
 
-pages = ('index', 'about')
 
 def make_sure_path_exists(path):
     try:
@@ -19,29 +18,38 @@ def make_sure_path_exists(path):
 
 def serve_static_site():
     # Serve the output directory
+    os.chdir('output/')
     PORT = 9090
     Handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("", PORT), Handler)
     print("serving at port", PORT)
     httpd.serve_forever()
 
-env = Environment()
-env.loader = FileSystemLoader('input/')
+def generate_html(pages):
+    env = Environment()
+    env.loader = FileSystemLoader('input/')
 
-for page in pages:
-    tmpl = env.get_template('{0}.html'.format(page))
-    rendered_html = tmpl.render()
+    for page in pages:
+        tmpl = env.get_template('{0}.html'.format(page))
+        rendered_html = tmpl.render()
 
-    # Put index in the root. It's a special case.
-    if page == 'index':
-        with open('output/index.html','w') as fh:
-            fh.write(rendered_html)
+        # Put index in the root. It's a special case.
+        if page == 'index':
+            with open('output/index.html','w') as fh:
+                fh.write(rendered_html)
 
-    # Put other pages in page/index.html, for better URL formatting.
-    else:
-        make_sure_path_exists('output/{0}/'.format(page))
-        with open('output/{0}/index.html'.format(page),'w') as fh:
-            fh.write(rendered_html)
+        # Put other pages in page/index.html, for better URL formatting.
+        else:
+            make_sure_path_exists('output/{0}/'.format(page))
+            with open('output/{0}/index.html'.format(page),'w') as fh:
+                fh.write(rendered_html)
 
-os.chdir('output/')
-serve_static_site()
+
+if __name__ == '__main__':
+
+    # TODO: automatically determine this from input/
+    pages = ('index', 'about')
+
+    # Generate and serve the HTML site
+    generate_html(pages)
+    serve_static_site()
