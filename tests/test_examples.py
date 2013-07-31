b76df1a5c4b874558d3e7dc0b0e6b93705a0ae49
Audrey Roy <audreyr@gmail.com>
2013-07-31 23:29:13 +0200

    Break TestExample up to use setUp/tearDown.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -15,17 +15,22 @@ import unittest
 from complexity.main import complexity
 
 
-class TestExamples(unittest.TestCase):
+class TestExample(unittest.TestCase):
 
+    def setUp(self):
+        os.system('git clone https://github.com/audreyr/complexity-example.git')
+        
     def test_complexity_example(self):
         """
         Tests that https://github.com/audreyr/complexity-example.git works.
         """
-        os.system('git clone https://github.com/audreyr/complexity-example.git')
+        
         complexity('complexity-example/project/', 'complexity-example/www/')
         self.assertTrue(os.path.isfile('complexity-example/www/index.html'))
         self.assertTrue(os.path.isfile('complexity-example/www/about/index.html'))
         self.assertTrue(os.path.isfile('complexity-example/www/img/glyphicons-halflings.png'))
+
+    def tearDown(self):
         shutil.rmtree('complexity-example')
 
 if __name__ == '__main__':
