51262b711bbb3e628e68ff03126bf23eef84c52f
Audrey Roy <audreyr@gmail.com>
2013-08-09 23:53:31 +0200

    Test conf_proj2, which has crazy config in complexity.json.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,12 +8,16 @@ test_main
 Tests for `complexity.main` module.
 """
 
+import logging
 import os
 import shutil
 import unittest
 
 from complexity import main
 
+
+# Log debug and above to console
+logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
 
 class TestMain(unittest.TestCase):
     
@@ -32,6 +36,17 @@ class TestConfProj(unittest.TestCase):
     def tearDown(self):
         if os.path.isdir('tests/www'):
             shutil.rmtree('tests/www')
+
+class TestConfProj2(unittest.TestCase):
+        
+    def test_conf_proj2_with_complexity(self):
+        main.complexity('tests/conf_proj2')
+        self.assertTrue(os.path.isfile('tests/conf_proj2/wwwz/index.html'))
+        self.assertTrue(os.path.isfile('tests/conf_proj2/wwwz/about/index.html'))
+
+    def tearDown(self):
+        if os.path.isdir('tests/conf_proj2/wwwz'):
+            shutil.rmtree('tests/conf_proj2/wwwz')
 
 
 if __name__ == '__main__':
