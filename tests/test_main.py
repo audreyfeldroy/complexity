3900df835bc7743c6ceb1bbf77a9b2a1a13dfcf4
Audrey Roy <audreyr@gmail.com>
2013-08-09 22:44:30 +0200

    Note to self about needing to test usage of argparse.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,14 +8,17 @@ test_main
 Tests for `complexity.main` module.
 """
 
+import os
+import shutil
 import unittest
 
 from complexity import main
 
 
 class TestMain(unittest.TestCase):
-
-    def test_something(self):
+    
+    def test_get_complexity_args(self):
+        """ TODO: figure out how to test argparse here. """
         pass
 
 if __name__ == '__main__':
